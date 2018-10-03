4bb613872712f8d7150443397b42297136a5f9c2
Victor Gomez Garcia <victorgomez@MacBook-Pro-de-Victor.local>
2018-10-03 18:26:41 +0200

    “TravisCI ahora crea imágenes con los binarios

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+#no lanzar el .jar hasta que no esté accesible el servicio de MongoDB
+#el host ‘mongo’ tendrá una IP otorgada al lanzar la pila de #servicios, y le damos
+el apodo ‘mongo’, utilizado aquí y en el #archivo application.properties
 
-while ! curl http://mongo:27017/
+While ! curl http://mongo:27017/
 do
-  echo "$(date) - still trying"
-  sleep 1
+ echo "$(date) - still trying"
+ sleep 1
 done
-echo "$(date) - connected successfully"
-
-java -jar ordering-*.jar
+echo "$(date) - it seems mongo server is connected successfully"
+java -jar usr/local/app/ordering-service-0.1.0.jar \\
+	 -spring.config.location=application.properties # o *.jar
