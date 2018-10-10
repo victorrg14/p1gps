7140fb1c16df0e863d52a99aa66db18ea5ee5e75
Victor Gomez Garcia <victorgomez@MacBook-Pro-de-Victor.local>
2018-10-10 13:22:37 +0200

    run.sh arreglado

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #no lanzar el .jar hasta que no esté accesible el servicio de MongoDB
-#el host ‘mongo’ tendrá una IP otorgada al lanzar la pila de #servicios, y le damos
-el apodo ‘mongo’, utilizado aquí y en el #archivo application.properties
+#el host ‘mongo’ tendrá una IP otorgada al lanzar la pila de #servicios, y le damos el apodo ‘mongo’, utilizado aquí y en el #archivo application.properties
 
-While ! curl http://mongo:27017/
+while ! curl http://mongo:27017/
 do
  echo "$(date) - still trying"
  sleep 1
